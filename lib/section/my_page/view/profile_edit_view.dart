3f98d7925ddca01aa7ca904621cc4f841f37cf36
황유림 <yulim00610@naver.com>
2024-07-29 00:26:19 +0900

    my page에 여행 플랜 연결 완 했으니 푸쉬

--- lib/section/my_page/view/profile_edit_view.dart
+++ lib/section/my_page/view/profile_edit_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project_island/section/feed/view/add_photo_view.dart'; // add_photo_view 파일 가져오기
+import 'package:project_island/section/feed/view/upload_view.dart'; // add_photo_view 파일 가져오기
 
 class ProfileEditView extends StatelessWidget {
   @override
@@ -22,7 +22,7 @@ class ProfileEditView extends StatelessWidget {
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => AddPhotoView()), // add_photo_view 페이지로 이동
+                  MaterialPageRoute(builder: (context) => UploadView()), // add_photo_view 페이지로 이동
                 );
               },
               child: CircleAvatar(

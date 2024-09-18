d1d7b4260432cdbccb68d87400f36bf5713fa0ee
황유림 <yulim00610@naver.com>
2024-09-18 17:34:49 +0900

    custom appbar에 커서 색깔 검정색으로 바꿨고 바탕 누르면 키보드 사라지게 했고 homemap_list 쪽에 길어서 가독성 떨어지니까 좀 나눴어유. 앱바 스크롤하면 연보라되는것도 수정함!

--- lib/section/map/view/homemap_list.dart
+++ lib/section/map/view/homemap_list.dart
@@ -5,7 +5,6 @@ import 'package:project_island/section/map/view/homemap_listview.dart';
 import 'package:project_island/section/map/widget/custom_appbar.dart';
 import 'package:project_island/section/map/widget/category_buttons.dart';
 
-
 class HomemapList extends StatefulWidget {
   final String islandName;
 
@@ -17,15 +16,13 @@ class HomemapList extends StatefulWidget {
 
 class HomemapListState extends State<HomemapList> {
   final HomemapListController controller = Get.put(HomemapListController());
-  // DraggableScrollableController를 선언하여 바텀시트의 높이를 제어합니다.
   final DraggableScrollableController draggableScrollableController = DraggableScrollableController();
 
   @override
   void initState() {
     super.initState();
-    // 초기 카테고리를 설정하고 데이터를 로드합니다.
-    controller.onCategorySelected(widget.islandName);
-    controller.loadInitialItems(widget.islandName);
+    controller.onCategorySelected(widget.islandName); // 초기 카테고리 설정
+    controller.loadInitialItems(widget.islandName); // 초기 데이터 로드
   }
 
   @override
@@ -36,167 +33,207 @@ class HomemapListState extends State<HomemapList> {
         onSearchSubmitted: controller.onSearchSubmitted, // ViewModel에서 처리
       ),
       backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          // 네이버 맵을 배경에 표시합니다.
-          Positioned.fill(
-            child: Container(
-              color: Colors.blueGrey,
-              child: const Center(child: Text('네이버 맵', style: TextStyle(color: Colors.white))),
+      // body를 GestureDetector로 감싸서 빈 공간 터치 시 키보드를 해제합니다.
+      body: GestureDetector(
+        behavior: HitTestBehavior.translucent, // 투명한 영역도 터치 이벤트 감지
+        onTap: () {
+          FocusScope.of(context).unfocus(); // 키보드 해제
+        },
+        child: Stack(
+          children: [
+            const MapBackground(), // 배경에 네이버 맵 표시
+            Column(
+              children: [
+                // Container로 감싸고 배경색을 흰색으로 설정합니다.
+                Container(
+                  color: Colors.white, // 상위 카테고리 바의 배경색을 흰색으로 설정
+                  child: Obx(() => CategoryButtons(
+                    selectedCategory: controller.selectedCategory.value,
+                    onCategorySelected: controller.onCategorySelected, // ViewModel에서 처리
+                  )),
+                ),
+                Expanded(
+                  child: DraggableScrollableSheet(
+                    controller: draggableScrollableController,
+                    initialChildSize: controller.isFullScreen.value ? 1.0 : 0.4,
+                    minChildSize: 0.4,
+                    maxChildSize: 1.0,
+                    expand: true,
+                    builder: (BuildContext context, ScrollController scrollController) {
+                      return BottomSheetContent(
+                        controller: controller,
+                        scrollController: scrollController,
+                        draggableController: draggableScrollableController,
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          ),
-          // 바텀시트와 카테고리 버튼 등을 포함하는 컬럼 위젯입니다.
-          Column(
-            children: [
-              // 카테고리 버튼들을 표시합니다.
-              Container(
-                color: Colors.white,
-                child: Obx(() => CategoryButtons(
-                  selectedCategory: controller.selectedCategory.value,
-                  onCategorySelected: controller.onCategorySelected, // ViewModel에서 처리
-                )),
-              ),
-              // DraggableScrollableSheet를 사용하여 바텀시트를 구현합니다.
-              Expanded(
-                child: DraggableScrollableSheet(
-                  controller: draggableScrollableController, // 컨트롤러를 지정합니다.
-                  initialChildSize: controller.isFullScreen.value ? 1.0 : 0.4, // 초기 높이 설정
-                  minChildSize: 0.4, // 최소 높이
-                  maxChildSize: 1.0, // 최대 높이
-                  expand: true,
-                  builder: (BuildContext context, ScrollController scrollController) {
-                    // 스크롤 컨트롤러는 리스트뷰에 전달됩니다.
+          ],
+        ),
+      ),
+      floatingActionButton: Obx(
+            () => controller.isFullScreen.value
+            ? FloatingMapButton(
+          onPressed: () {
+            controller.isFullScreen.value = false; // fullscreen 상태 해제
+            // 바텀시트 높이를 0.4로 조정
+            draggableScrollableController.animateTo(
+              0.4,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+            );
+          },
+        )
+            : const SizedBox.shrink(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
 
-                    // 바텀시트의 확장 정도를 모니터링하기 위해 NotificationListener를 사용합니다.
-                    return NotificationListener<DraggableScrollableNotification>(
-                      onNotification: (notification) {
-                        // 바텀시트가 완전히 확장되었을 때
-                        if (notification.extent == 1.0 && !controller.isFullScreen.value) {
-                          setState(() {
-                            controller.isFullScreen.value = true; // fullscreen 상태로 변경
-                          });
-                        }
-                        // 바텀시트가 축소되었을 때
-                        else if (notification.extent < 1.0 && controller.isFullScreen.value) {
-                          setState(() {
-                            controller.isFullScreen.value = false; // fullscreen 상태 해제
-                          });
-                        }
-                        return true;
-                      },
-                      child: Obx(
-                            () => Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: controller.isFullScreen.value
-                                ? BorderRadius.zero
-                                : BorderRadius.vertical(top: Radius.circular(20)),
-                          ),
-                          child: Column(
-                            children: [
-                              // 바텀시트가 fullscreen이 아닐 때 손잡이 표시
-                              if (!controller.isFullScreen.value)
-                                Container(
-                                  width: 40,
-                                  height: 6,
-                                  margin: const EdgeInsets.symmetric(vertical: 10),
-                                  decoration: BoxDecoration(
-                                    color: Colors.grey[300],
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                ),
-                              // 선택된 카테고리가 '관심'일 때 서브 카테고리 버튼들을 표시합니다.
-                              if (controller.selectedCategory.value == '관심') ...[
-                                Divider(color: Colors.grey[200], thickness: 1, height: 0),
-                                SubCategoryButtons(
-                                  selectedSubCategory: controller.selectedSubCategory.value,
-                                  onSubCategorySelected: controller.onSubCategorySelected, // ViewModel에서 처리
-                                ),
-                              ],
-                              Divider(color: Colors.grey[200], thickness: 1, height: 5),
-                              // 목록의 개수를 표시합니다.
-                              Padding(
-                                padding: const EdgeInsets.only(left: 20, top: 5, bottom: 10),
-                                child: Align(
-                                  alignment: Alignment.centerLeft,
-                                  child: Row(
-                                    children: [
-                                      const Text(
-                                        '목록 ',
-                                        style: TextStyle(
-                                            color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold),
-                                      ),
-                                      Text(
-                                        '${controller.displayedItems.length}개',
-                                        style: const TextStyle(
-                                            color: Colors.green, fontSize: 14, fontWeight: FontWeight.bold),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                              // 리스트뷰를 표시합니다.
-                              Expanded(
-                                child: controller.displayedItems.isEmpty
-                                    ? const Center(child: Text('저장된 항목이 없습니다.'))
-                                    : HomemapListView(
-                                  items: controller.displayedItems,
-                                  controller: controller,
-                                  scrollController: scrollController, // 리스트뷰에 스크롤 컨트롤러 전달
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+// 나머지 위젯들은 이전과 동일합니다...
+
+
+// 배경에 네이버 맵을 표시하는 위젯
+class MapBackground extends StatelessWidget {
+  const MapBackground({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: Container(
+        color: Colors.blueGrey,
+        child: const Center(
+          child: Text(
+            '네이버 맵',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// 바텀시트의 내용을 표시하는 위젯
+class BottomSheetContent extends StatelessWidget {
+  final HomemapListController controller;
+  final ScrollController scrollController;
+  final DraggableScrollableController draggableController;
+
+  const BottomSheetContent({
+    Key? key,
+    required this.controller,
+    required this.scrollController,
+    required this.draggableController,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return NotificationListener<DraggableScrollableNotification>(
+      onNotification: (notification) {
+        if (notification.extent == 1.0 && !controller.isFullScreen.value) {
+          controller.isFullScreen.value = true;
+        } else if (notification.extent < 1.0 && controller.isFullScreen.value) {
+          controller.isFullScreen.value = false;
+        }
+        return true;
+      },
+      child: Obx(
+            () => Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: controller.isFullScreen.value
+                ? BorderRadius.zero
+                : const BorderRadius.vertical(top: Radius.circular(20)),
+          ),
+          child: Column(
+            children: [
+              if (!controller.isFullScreen.value)
+                Container(
+                  width: 40,
+                  height: 6,
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              if (controller.selectedCategory.value == '관심') ...[
+                Divider(color: Colors.grey[200], thickness: 1, height: 0),
+                SubCategoryButtons(
+                  selectedSubCategory: controller.selectedSubCategory.value,
+                  onSubCategorySelected: controller.onSubCategorySelected,
+                ),
+              ],
+              Divider(color: Colors.grey[200], thickness: 1, height: 5),
+              Padding(
+                padding: const EdgeInsets.only(left: 20, top: 5, bottom: 10),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Row(
+                    children: [
+                      const Text(
+                        '목록 ',
+                        style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold),
                       ),
-                    );
-                  },
+                      Text(
+                        '${controller.displayedItems.length}개',
+                        style: const TextStyle(color: Colors.green, fontSize: 14, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: controller.displayedItems.isEmpty
+                    ? const Center(child: Text('저장된 항목이 없습니다.'))
+                    : HomemapListView(
+                  items: controller.displayedItems,
+                  controller: controller,
+                  scrollController: scrollController,
                 ),
               ),
             ],
           ),
-        ],
+        ),
       ),
-      // floatingActionButton을 사용하여 '지도보기' 버튼을 표시합니다.
-      floatingActionButton: Obx(
-            () => controller.isFullScreen.value
-            ? Align(
-          alignment: Alignment.bottomCenter,
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 20),
-            child: GestureDetector(
-              onTap: () {
-                // '지도보기' 버튼을 눌렀을 때 바텀시트의 높이를 0.4로 줄입니다.
-                controller.isFullScreen.value = false; // fullscreen 상태 해제
+    );
+  }
+}
 
-                // DraggableScrollableController를 사용하여 바텀시트의 높이를 0.4로 설정합니다.
-                draggableScrollableController.animateTo(
-                  0.4, // 원하는 높이 (0.4)
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.easeInOut,
-                );
-              },
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(Icons.pin_drop_sharp, color: Colors.white, size: 18),
-                    const SizedBox(width: 8),
-                    const Text('지도보기', style: TextStyle(color: Colors.white, fontSize: 18)),
-                  ],
-                ),
-              ),
+// '지도보기' 버튼을 표시하는 위젯
+class FloatingMapButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const FloatingMapButton({Key? key, required this.onPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 20),
+        child: GestureDetector(
+          onTap: onPressed,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Icon(Icons.pin_drop_sharp, color: Colors.white, size: 18),
+                SizedBox(width: 8),
+                Text('지도보기', style: TextStyle(color: Colors.white, fontSize: 18)),
+              ],
             ),
           ),
-        )
-            : const SizedBox.shrink(), // '지도보기' 버튼을 표시하지 않을 때 빈 위젯 반환
+        ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
